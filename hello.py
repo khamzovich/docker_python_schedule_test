6fae9bdfd4eceeb09924dde0882998ef28c5ca18
khamzovich <xxamz@yandex.ru>
2022-06-15 20:50:24 +0300

    2

--- hello.py
+++ hello.py
@@ -1,15 +1,3 @@
-# from itertools import chain
-# import re
-# from google.oauth2 import service_account
-
-# mongo
-# import paramiko
-# from paramiko import SSHClient
-# from sshtunnel import SSHTunnelForwarder
-# from pymongo import MongoClient
-
-# from user_agents import parse
-
 import telebot  # для работы с телеграм ботом
 import pandas as pd
 import numpy as np
