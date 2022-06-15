d52ec2d20d48fba2bdbac190ac03b028550142ce
khamzovich <xxamz@yandex.ru>
2022-06-15 22:33:39 +0300

    3

--- hello.py
+++ hello.py
@@ -2,22 +2,26 @@ import telegram
 
 from datetime import datetime
 import pytz
-from settings import TG_TOKEN, USER_ID
+# from settings import TG_TOKEN, USER_ID
+
+# from github import Github # PyGithub
+import os
+
+telegram_token = os.environ['TG_TOKEN']
+user_id = 200428870
 
 
 def test_report(chat_id, telegram_token, msg):
-
     bot = telegram.Bot(token=telegram_token)
-    
     # send text message
     bot.sendMessage(chat_id=chat_id, text=msg)
 
-IST = pytz.timezone('Europe/Moscow')
 
+IST = pytz.timezone('Europe/Moscow')
 # datetime_ist = datetime.now(IST)
 message = datetime.now(IST).strftime('%Y-%m-%d %H:%M:%S')
 
 try:
-    test_report(USER_ID, TG_TOKEN, message)
+    test_report(user_id, telegram_token, message)
 except Exception as e:
     print(e)
