178d6faa6b5124244076ce754a0628416c7c1362
khamzovich <xxamz@yandex.ru>
2022-06-15 20:47:40 +0300

    1

--- hello.py
+++ hello.py
@@ -1,7 +1,3 @@
-import telebot  # для работы с телеграм ботом
-import pandas as pd
-import numpy as np
-
 # from itertools import chain
 # import re
 # from google.oauth2 import service_account
@@ -14,18 +10,20 @@ import numpy as np
 
 # from user_agents import parse
 
+import telebot  # для работы с телеграм ботом
+import pandas as pd
+import numpy as np
+
 from datetime import datetime
 import pytz
+from settings import TG_TOKEN, USER_ID
 
 IST = pytz.timezone('Europe/Moscow')
 
 # datetime_ist = datetime.now(IST)
 x = datetime.now(IST).strftime('%Y-%m-%d %H:%M:%S')
 
-telegram_token = '5157057758:AAEBmWYKKnBmnGiJxWGthmrPyXsqGi5sc5c'
-user_id = 200428870
-
-bot = telebot.TeleBot(telegram_token)
-bot.send_message(user_id, f'{df.shape} - {x}')
+bot = telebot.TeleBot(TG_TOKEN)
+bot.send_message(USER_ID, f'{x}')
 
 print(x)
