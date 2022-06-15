1d5a724e831f9875c1b97e6243668bd870593887
khamzovich <xxamz@yandex.ru>
2022-06-15 21:26:56 +0300

    2

--- hello.py
+++ hello.py
@@ -1,17 +1,23 @@
-import telebot  # для работы с телеграм ботом
-import pandas as pd
-import numpy as np
+import telegram
 
 from datetime import datetime
 import pytz
 from settings import TG_TOKEN, USER_ID
 
+
+def test_report(chat_id, telegram_token, msg):
+
+    bot = telegram.Bot(token=telegram_token)
+    
+    # send text message
+    bot.sendMessage(chat_id=chat_id, text=msg)
+
 IST = pytz.timezone('Europe/Moscow')
 
 # datetime_ist = datetime.now(IST)
-x = datetime.now(IST).strftime('%Y-%m-%d %H:%M:%S')
+message = datetime.now(IST).strftime('%Y-%m-%d %H:%M:%S')
 
-bot = telebot.TeleBot(TG_TOKEN)
-bot.send_message(USER_ID, f'{x}')
-
-print(x)
+try:
+    test_report(USER_ID, TG_TOKEN, message)
+except Exception as e:
+    print(e)
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-exec python3 /app/hello.py &
-exec python3 /app/article_stat.py
