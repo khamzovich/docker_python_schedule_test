e4de8253171600002d5ae02da428f67d0e274a9a
Emil <xxamz@yandex.ru>
2022-06-16 15:25:20 +0300

    Update hello.py
    git

--- hello.py
+++ hello.py
@@ -7,9 +7,12 @@ import pytz
 import os
 
 
-telegram_token = os.environ['TG_TOKEN']
-user_id = os.environ['USER_ID']
+# telegram_token = os.environ['TG_TOKEN']
+# user_id = os.environ['USER_ID']
 # g_bq = os.environ['G_BQ']
+
+telegram_token = os.environ.get('TG_TOKEN')
+user_id = os.environ.get('USER_ID')
 
 
 def test_report(chat_id, telegram_token, msg):
@@ -36,7 +39,7 @@ def test_report(chat_id, telegram_token, msg):
 
 # n_users = str(n_users_df.iloc[0, 0])
 
-n_users = '54'
+n_users = ' 54'
 
 
 IST = pytz.timezone('Europe/Moscow')
