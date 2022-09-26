1ec556e1e294e4b9dfc4d023469f5a2d89c7ed3b
Emil <xxamz@yandex.ru>
2022-09-26 14:24:04 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -21,24 +21,6 @@ def test_report(chat_id, telegram_token, msg):
     bot.sendMessage(chat_id=chat_id, text=msg)
 
 
-# credentials = service_account.Credentials.from_service_account_info(g_bq)
-# project_id = 'medpoint-gbq'
-
-
-# query = f"""
-#          SELECT
-#             COUNT(DISTINCT user_id) AS n_users
-#          FROM `medpoint-gbq.analytics_250521411.user_info`
-#          """
-
-# # max saved event date
-# n_users_df = pd.read_gbq(query,
-#                          project_id=project_id,
-#                          credentials=credentials,
-#                          max_results=10)
-
-# n_users = str(n_users_df.iloc[0, 0])
-
 n_users = ' 54'
 
 
